b4e095a83bfca348a5490ac59a16176912506f3f
Hector Zarco <hezarco@gmail.com>
2014-08-08 19:49:23 +0200

    Mocking data

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,5 +1,66 @@
 import Ember from "ember";
 
-export default Em.Route.extend({
+export
+default Em.Route.extend({
+  beforeModel: function() {
+    //FAKE DATA FOR DEVELOPMENT
+    var brands = ['ON', 'Dymatize', 'Weider'];
+    var types = ['protein', 'casein', 'creatine', 'bcca', 'l-carnitine', 'glutamine'];
+    var supplements = [{
+      brand: 'ON',
+      type: 'protein'
+    }, {
+      brand: 'ON',
+      type: 'casein'
+    }, {
+      brand: 'ON',
+      type: 'creatine'
+    }, {
+      brand: 'Dymatize',
+      type: 'protein'
+    }, {
+      brand: 'Dymatize',
+      type: 'casein'
+    }, {
+      brand: 'Dymatize',
+      type: 'creatine'
+    }, {
+      brand: 'Dymatize',
+      type: 'bcca'
+    }, {
+      brand: 'Weider',
+      type: 'protein'
+    }, {
+      brand: 'Weider',
+      type: 'l-carnitine'
+    }, {
+      brand: 'Weider',
+      type: 'glutamine'
+    }, {
+      brand: 'Weider',
+      type: 'bcca'
+    }];
 
+    types.forEach(function(type) {
+      this.get('store').createRecord('supplementType', {
+        id: type,
+        name: type
+      });
+    }, this);
+
+    brands.forEach(function(brand) {
+      this.get('store').createRecord('brand', {
+        id: brand,
+        name: brand
+      });
+    }, this);
+
+    supplements.forEach(function(supplement) {
+      this.get('store').createRecord('supplement', {
+        id: supplement.brand + '-' + supplement.type,
+        brand: this.get('store').getById('brand', supplement.brand),
+        type: this.get('store').getById('supplementType', supplement.type)
+      });
+    }, this);
+  }
 });
